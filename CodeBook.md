1ef0069f90cf990c917f9911503c61d030b703ea
“IzableaEdmunds” <izabelabeatanowak@googlemail.com>
2015-05-24 18:44:32 +0100

    my project files

---
+++ CodeBook.md
@@ -0,0 +1,41 @@
+# Getting and Cleaning Data Course Project CodeBook
+## This code book describes the work performed to clean up the data and additional columns in two files created by the script. 
+The whole transformations/work is done by “run_analysis.R” script so no manual steps are necessary (it takes around 1 minute to run together with downloading the file). 
+Please read README.md to see the script and its detailed description.
+** Please note English is not my first language, so I do apologise for any spelling/grammar mistakes and hope all reads clear and is understandable.
+## Source Files 
+The data was obtained from: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+The description of the original data can be found at: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+The files in zip archive used for this analysis, as per each directory:
+* features_info.txt: Shows information about the variables used on the feature vector.
+* features.txt - List of all features.
+* activity_labels.txt - Links the class labels with their activity name.
+* train/X_train.txt - Training set.
+* train/y_train.txt - Training labels.
+* test/X_test.txt - Test set.
+* test/y_test.txt - Test labels.
+* train/subject_train.txt - Each row identifies the subject who performed the activity for each window sample.
+* test/subject_train.txt - Each row identifies the subject who performed the activity for each window sample.
+## Transformations
+The whole transformations/work is done by “run_analysis.R” script so no manual steps are necessary (it takes around 1 minute to run together with downloading the file). 
+Here I have described simply how “run_analysis.R” script works (details in README.md):
+1) The scripts first checks if our working directory has a folder named “data” and if not, it creates a folder named “data”. 
+2) Then sets URL target as variable "fileUrl" for the site with the data for the project.
+3) Then downloads the file to "data" directory as "download.zip" and records date and time of the download as variable “dateDownloaded” and prints date and time of the download to console.
+4) Then it unzips the file into “data” directory into directory “UCI HAR Dataset” (as per zip file content).
+5) Then the script follows the 5 steps of the assignment:
+* Merges the training and the test data sets to create one data set.
+* Extracts only the measurements on the mean and standard deviation for each measurement.
+* Renames activity names to name the activities in the data set to be descriptive.
+* Labels appropriately the data set with descriptive variable names. 
+* From the last data set (in step 4), it creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+6) Both files (“measurements_mean_std.txt” and “activity_subject_means.txt”) are saved by write.table() in “data” folder. They should be read by using read.table()
+
+# The script’s output data
+## The columns in “measurements_mean_std.txt” is as follows:
+* Activity - descriptive label of activity
+all the other columns list the measurements as in the “features_info.txt” file in the original raw data set. 
+## The columns in “activity_subject_means.txt” is as follows:
+* activity - descriptive label of activity
+* subject - test subject id
+all the other columns list the measurements as in the “features_info.txt” file in the original raw data set. 
---
+++ run_analysis.R
@@ -0,0 +1,102 @@
+## Check if "data" directory exists
+if (!file.exists("data")) { 
+    ## if directory does not exist create a directory "data" in the working directory
+    dir.create("data") 
+}
+
+
+## Set URL target as variable "fileUrl"
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+
+## Download to "data" directory as "download.zip" with no "curl" as I am running this script on Windows 7
+download.file(fileUrl, destfile = "./data/download.zip") 
+
+
+# Record date and time of the download as an object "dateDownloaded"
+dateDownloaded <- date()
+# Print date and time of the download
+print(dateDownloaded) 
+
+
+## Unzip files
+unzip("./data/download.zip", exdir = "./data")
+
+
+## load library for "Reshape Grouped Data"
+library(reshape2)
+
+
+
+## 1. Merge the training and the test sets to create one data set.
+
+# Read each table into one variable from "train" and "test" folder by "x", "y" and "subject"
+x_train_data <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+x_test_data <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+y_train_data <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+y_test_data <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_train_data <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+subject_test_data <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+
+# Merge data sets to create one dataset for each "x", "y" and "subject".
+joint_x <- rbind(x_train_data, x_test_data)
+joint_y <- rbind(y_train_data, y_test_data)
+joint_subject <- rbind(subject_train_data, subject_test_data)
+
+
+
+## 2. Extract only the measurements on the mean and standard deviation for each measurement. 
+
+# Read features into a table as variable "features"
+features <- read.table("./data/UCI HAR Dataset/features.txt")[,"V2"]
+
+# set descriptive names for all columns in merged "x", "y" and "subject"
+colnames(joint_x) <- features
+colnames(joint_y) <- "activity_ID"
+colnames(joint_subject) <- "subject"
+
+# select features containing measurements on the mean and standard deviation
+means <- grep("-mean\\(\\)", features, value=TRUE)
+std_dev <- grep("-std\\(\\)", features, value=TRUE)
+measurements <- c(means, std_dev)
+new_x <- joint_x[, measurements]
+
+
+
+## 3. Use descriptive activity names to name the activities in the dataset
+
+# Read activity descriptions into a table as variable "activities"
+activities <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+
+# Set the column names in "activities"
+colnames(activities) <- c("activity_ID", "activity")
+
+# join activity dataset with descriptive labels
+new_y <- merge(joint_y, activities)
+
+
+
+## 4. Appropriately labels the data set with descriptive activity names.
+
+# Join measurements dataset with activity labels
+data1 <- cbind(new_x, new_y["activity"])
+
+# Write the resultant dataset to a table in "data" folder
+write.table(data1, "./data/measurements_mean_std.txt")
+
+# Join subject IDs
+data2 <- cbind(data1, joint_subject)
+
+
+
+## 5. From the data set in step 4 (data2), create a second, independent tidy data set 
+## with the average of each variable for each activity and each subject.
+
+# Summarise means per activity and subject
+data2_melt <- melt(data2, id=c("subject", "activity"))
+
+# create the average
+data3 <- dcast(data2_melt, activity + subject ~ variable, mean)
+
+# write second result to a table in "data" folder
+write.table(data3, "./data/activity_subject_means.txt", row.name=FALSE)
